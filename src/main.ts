fdfcd858b0349c4a3db4bb61faf028c62139861b
drumath2237 <2237k.drumath@gmail.com>
2023-04-16 12:33:12 +0900

    :package: babylonのインポートと設定

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
+import { Engine, Scene, MeshBuilder } from "@babylonjs/core";
 import "./style.scss";
 
 const main = () => {
-  const renderCanvas = document.getElementById("renderCanvas");
+  const renderCanvas = document.getElementById(
+    "renderCanvas"
+  ) as HTMLCanvasElement;
   if (!renderCanvas) {
     return;
   }
 
-  console.log("hello");
+  renderCanvas.width = document.firstElementChild?.clientWidth ?? 0;
+  renderCanvas.height = document.firstElementChild?.clientHeight ?? 0;
+
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+
+  MeshBuilder.CreateBox("box", { size: 0.5 }, scene);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 };
 
 main();
